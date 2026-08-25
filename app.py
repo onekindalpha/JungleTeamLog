262b5cc80b54f9c88432304c4e29cb7937357539
jye-1123 <jyepjh1123@gmail.com>
2026-08-25 23:56:37 +0900

    auth2
    auth2

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-# app.py: 앱 생성 + .env 로드 + 각 담당자가 만든 blueprint들을 모아서 등록
-
 from flask import Flask
 from dotenv import load_dotenv
 from routes.team import team_blueprints
-
-from flask import Flask, redirect, url_for, render_template, request
-from dotenv import load_dotenv
 from routes.auth import auth_bp
-from routes.utils.jwt_utils import decode_token
-
-# app.py: 앱 생성 + .env 로드 + 각 담당자가 만든 blueprint들을 모아서 등록
 
 
 load_dotenv() # .env 파일 읽기
@@ -17,16 +9,12 @@ load_dotenv() # .env 파일 읽기
 app = Flask(__name__) # Flask 애플리케이션 객체를 하나 생성
 # __name__은 "지금 이 파일이 뭔지" 파이썬이 자동으로 넣어주는 값
 
-
 for bp in team_blueprints:
     app.register_blueprint(bp)
-
+    
 app.register_blueprint(auth_bp)
 
 
 # "이 파일을 직접 실행했을 때만" 아래 코드를 실행하라는 뜻
 if __name__ == "__main__":
     app.run(debug=True) # 실제로 서버를 켜서 요청을 기다리기 시작 (http://127.0.0.1:5000)
-    # debug=True : 개발 중에 편하라고 켜두는 옵션
-    # 코드를 수정하고 저장하면 서버가 자동으로 재시작되고, 에러 나면 브라우저 화면에 자세한 에러 내용을 보여준다
-    # 배포할 때는 보안상 False로 바꾸거나 아예 빼야 함
--- config.py
+++ config.py
@@ -2,16 +2,8 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-
 load_dotenv()
 
 client = MongoClient(os.getenv("MONGODB_URI"))
 db = client[os.getenv("DB_NAME", "jungle_teamlog")]
-
-# MongoDB 연결
-client = MongoClient("mongodb://localhost:27017/")
-db = client["jungle_teamlog"]
-
-load_dotenv()
-
 SECRET_KEY = os.getenv("SECRET_KEY")
